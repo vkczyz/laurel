41487be4c6f5b0a84baf1b67ca12fa3d8d366f8c
mzyzc <matthew@czyz.xyz>
2022-07-06 22:52:20 +0100

    Removed placeholder user pages

--- users.php
+++ users.php
@@ -1,9 +1,22 @@
 <?php include "templates/header.html"; ?>
 
+<?php $user = $_GET["id"]; ?>
+
 <h2>User</h2>
 
-<h3><?php echo $_GET["id"]; ?></h3>
+<h3><?php echo $user; ?></h3>
 
-<p>Joined <?php echo date("Y-m-d H:i:s"); ?></p>
+<?php
+  $db = new SQLite3("data.db");
+  $stmt = $db->prepare("SELECT register_date FROM users WHERE username = :username");
+  $stmt->bindValue(':username', $user, SQLITE3_TEXT);
+  $register_date = $stmt->execute()->fetchArray()['register_date'];
+
+  if ($register_date) {
+    echo "<p>Joined " . date("Y-m-d H:i:s", $register_date) . " UTC</p>";
+  } else {
+    echo "<p>This user does not exist</p>";
+  }
+?>
 
 <?php include "templates/footer.html"; ?>
