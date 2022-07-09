898cf457d6bba6f12abde3eadb15f31d7ff4fc3d
mzyzc <matthew@czyz.xyz>
2022-07-09 15:22:27 +0100

    Added page navigation bar to both top and bottom of page

--- templates/page.php
+++ templates/page.php
@@ -1,7 +1,19 @@
 <?php
   $page = $_GET["page"] ?? 1;
   $limit = 10;
+?>
 
+<nav>
+  <a <?php
+    if ($page > 1) {
+      echo 'href="/index.php?page=' . ($page - 1) . '"';
+    }
+  ?>>⮜ Previous</a>
+  <span>Page <?php echo $page; ?></span>
+  <a href="/index.php?page=<?php echo ($page + 1); ?>">Next ➤</a>
+</nav>
+
+<?php
   $db = new SQLite3("data.db");
   $stmt = $db->prepare("SELECT id FROM posts WHERE parent = 0 ORDER BY publish_date DESC LIMIT :limit OFFSET :offset");
   $stmt->bindValue(":limit", $limit, SQLITE3_INTEGER);
@@ -22,4 +34,4 @@
   ?>>⮜ Previous</a>
   <span>Page <?php echo $page; ?></span>
   <a href="/index.php?page=<?php echo ($page + 1); ?>">Next ➤</a>
-</nav
+</nav>
