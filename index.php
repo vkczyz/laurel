d8d4ef597cbb338e75a372ce1c10ee579146cf5c
mzyzc <matthew@czyz.xyz>
2022-07-09 14:36:24 +0100

    Pagination now starts at 1 and pages before the first can no longer be accessed

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
     echo "<p>You must log in to post content</p>";
   }
 
-  $page = $_GET["page"] ?? 0;
+  $page = $_GET["page"] ?? 1;
   $limit = 10;
 
   $db = new SQLite3("data.db");
   $stmt = $db->prepare("SELECT id FROM posts WHERE parent = 0 ORDER BY publish_date DESC LIMIT :limit OFFSET :offset");
   $stmt->bindValue(":limit", $limit, SQLITE3_INTEGER);
-  $stmt->bindValue(":offset", $page * $limit, SQLITE3_INTEGER);
+  $stmt->bindValue(":offset", ($page - 1) * $limit, SQLITE3_INTEGER);
   $result = $stmt->execute();
 
   while ($data = $result->fetchArray()) {
@@ -26,7 +26,12 @@
 ?>
 
 <nav>
-  <a href="/index.php?page=<?php echo ($page - 1); ?>">⮜ Previous</a>
+  <a <?php
+    if ($page > 1) {
+      echo 'href="/index.php?page=' . ($page - 1) . '"';
+    }
+  ?>>⮜ Previous</a>
+  <span>Page <?php echo $page; ?></span>
   <a href="/index.php?page=<?php echo ($page + 1); ?>">Next ➤</a>
 </nav
 
